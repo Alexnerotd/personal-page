9681fb6d8d5d592eb10cf7e5ceeacbf049cb0e9e
Alex nero <jesalemalo@gmail.com>
2024-03-06 12:06:56 -0500

    Router provider created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
-import { useState } from "react";
 import "./App.css";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>Layout</p>,
+    errorElement: <p>ErrorElement</p>,
+    children: [
+      {
+        path: "home",
+        element: <p>Home</p>,
+        children: [
+          {
+            path: "about",
+            element: <p>About</p>,
+          },
+          {
+            path: "projects",
+            element: <p>Projects</p>,
+          },
+          {
+            path: "contact",
+            element: <p>Contact</p>,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
